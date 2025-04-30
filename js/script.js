4033bc7ea7c1d5f0b709d46b0eaa143ff06599cd
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-04-30 13:36:44 -0300

    Modificando os componentes 2

--- js/script.js
+++ js/script.js
@@ -18,6 +18,10 @@ if (toastTrigger) {
   });
 }
 
+/** Popovers */
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 /** Tooltips */
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
