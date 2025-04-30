c1008f3400b280f19fb937ea4fba017c502a30bc
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-04-30 08:45:36 -0300

    Adicionando componentes Bootstrap

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/** Modal */
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+});
+
+
+/** Toast */
+const toastTrigger = document.getElementById('liveToastBtn');
+const toastLiveExample = document.getElementById('liveToast');
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show();
+  });
+}
+
+/** Tooltips */
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+const tooltip = new bootstrap.Tooltip('#example', {
+    boundary: document.body // or document.querySelector('#boundary')
+  })
